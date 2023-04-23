e9b036becec2509fba3ba02ceb31972e8d8bfc37
John Kenneth Martinez <jkmartinez.career@gmail.com>
2023-04-23 21:58:30 +0800

    Removed identity check step.

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,12 +1,9 @@
 <template>
-	<!-- <router-view v-slot="{ Component }">
+	<router-view v-slot="{ Component }">
 		<transition name="fade">
 			<component :is="Component" />
 		</transition>
-	</router-view> -->
-
-	<router-view></router-view>
-	
+	</router-view>
 </template>
 
 <script>
